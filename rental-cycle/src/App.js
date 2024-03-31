f56502b06123baf8777466d58924509734fea057
#ASHME <darshannakshatri@gmail.com>
2024-03-31 17:33:32 +0530

    booking partly done

--- rental-cycle/src/App.js
+++ rental-cycle/src/App.js
@@ -1,11 +1,15 @@
-
+import Homepage from './pages/Homepage';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
+import Booking from './pages/Bookingpage';
 
 function App() {
   return (
     <div>
+      <Landing />
+      <Homepage />
       <Login />
+    <Booking />
     </div>
   );
 }
---
+++ rental-cycle/src/pages/Bookingpage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from '../components/Bookingpage.module.css'
+import img from '../assets/bookingpageimg.gif'
+import boxes from '../assets/bookingpageinput.jpg'
+
+
+function booking(){
+    return(
+        <>
+        <div className={styles.all}>
+            <div className={styles.title}>RIGHT PLACE TO BOOK YOUR JOURNEY</div>
+            <div className={styles.midpart}>
+                <img src={img} className={styles.gif}></img>
+                <div>
+                    <div className={styles.input1}>
+                        <div className={styles.from}>From: <input className={styles.from1}></input></div>
+                        <img src={boxes} className={styles.box1}></img>
+                    </div>
+                    <div className={styles.input2}>
+                        <div className={styles.to}>To: <input className={styles.to1}></input></div>
+                        <img src={boxes} className={styles.box2}></img>
+                    </div>
+                </div>
+                <div>
+                    <div className={styles.time}></div>
+                    <div className={styles.duration}></div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+export default booking
