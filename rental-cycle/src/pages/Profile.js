a58c9c3f04d5ed776ca1132902d8cfb043fada65
#ASHME <darshannakshatri@gmail.com>
2024-04-19 00:36:15 +0530

    half way through profile

--- rental-cycle/src/pages/Profile.js
+++ rental-cycle/src/pages/Profile.js
@@ -1,63 +1,11 @@
 import styles from '../components/Profile.module.css';
 import React, { PureComponent } from 'react';
 import img from '../assets/logout.jpg';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-
-
-
-const profile_data=[
-    {name: "Darshan", value:"darshannakshatri@gmail.com", balance:3000},
-];
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
 
 
 
 function Profile(){
-    const data2 = [
-        {
-          name: 'Page A',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
-        },
-      ];
     return(
         <>
           <header>
@@ -69,6 +17,30 @@ function Profile(){
                     </div>
                 </nav>
             </header>
+
+            <div className={styles.midpart}>
+                <div className={styles.box1}>
+                    <div className={styles.username1}>USER NAME</div>
+                    <div className={styles.email1}>darshannakshatri@gmail.com</div>
+                    <div className={styles.phDOB}>
+                        <div>Phone No</div>
+                        <div>Date of Birth</div>
+                    </div>
+                    <div className={styles.phDOB1}>
+                        <div>+9988766554</div>
+                        <div>30-02-2004</div>
+                    </div>
+                </div>
+                <div className={styles.box2}>
+                  <div className={styles.balance1}>Balance</div>
+                  <div className={styles.money1}>₹ 2000.00</div>
+                </div>
+
+            </div>
+
+            <div className={styles.bottomlist}>
+
+            </div>
        
 
         
