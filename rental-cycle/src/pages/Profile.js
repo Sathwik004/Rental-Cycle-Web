652cc0dfd4fa54b838b90d43374ebe035a37ac09
#ASHME <darshannakshatri@gmail.com>
2024-04-19 18:19:29 +0530

    progile done

--- rental-cycle/src/pages/Profile.js
+++ rental-cycle/src/pages/Profile.js
@@ -1,16 +1,55 @@
 import styles from '../components/Profile.module.css';
 import React, { PureComponent } from 'react';
 import img from '../assets/logout.jpg';
+import { useTable} from 'react-table';
 
-
+const data = [
+    {"datetime":"23" , "sou": "AB1", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹40"},
+    {"datetime":"24" , "sou": "AB2", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹50"},
+    {"datetime":"25" , "sou": "AB3", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹60"},
+    {"datetime":"26" , "sou": "AB4", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹70"},
+    {"datetime":"27" , "sou": "AB5", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹80"},
+    {"datetime":"23" , "sou": "AB1", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹40"},
+    {"datetime":"24" , "sou": "AB2", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹50"},
+    {"datetime":"25" , "sou": "AB3", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹60"},
+    {"datetime":"26" , "sou": "AB4", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹70"},
+    {"datetime":"27" , "sou": "AB5", "des":"Student Plaza", "dur": "10:21 min", "amt": "₹80"}
+]
 
 
 function Profile(){
+    const data1 = React.useMemo(() => data, []);
+    const col = React.useMemo(()=> [
+        {
+            Header: "Time Stamp",
+            accessor: "datetime"
+        },
+        {
+            Header: "Source",
+            accessor: "sou"
+        },
+        {
+            Header: "Destination",
+            accessor: "des"
+        },
+        {
+            Header: "Duration",
+            accessor: "dur"
+        },
+        {
+            Header: "Amount",
+            accessor: "amt"
+        },
+    ],
+    []
+);
+
+    const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow}=useTable({columns: col, data: data1});
     return(
         <>
           <header>
                 <nav className={styles.navdiv}>
-                    <div className={styles.title}>CYCLE HUB</div>
+                    <div className={styles.title}>Cyclehub</div>
                     <div>
                         <button className={styles.navbutton} >Home</button>
                         <button className={styles.navbutton} ><img src={img} className={styles.imgicon}></img>Logout</button>
@@ -38,10 +77,37 @@ function Profile(){
 
             </div>
 
-            <div className={styles.bottomlist}>
+            <div className={styles.container}>
+                    <table {...getTableProps()}>
+                        <thead className={styles.tabletitle}>
+                            {headerGroups.map((headerGroup)=>(
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map((col)=>(
+                                    <th {...col.getHeaderProps()}>
+                                        {col.render("Header")}
+                                    </th>
+                                ))}
+                            </tr>
+                            ))}
+                        </thead>
+                        <tbody {...getTableBodyProps()}>
+                            {rows.map((row) => {
+                                prepareRow(row)
+                                return(
+                                    <tr {...row.getRowProps()}>
+                                        {row.cells.map((cell) =>(
+                                            <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                                        ))}
+
+                                    </tr>
+                                )
+                            })}
+
+                        </tbody>
+                    </table>
 
             </div>
-       
+            
 
         
 
